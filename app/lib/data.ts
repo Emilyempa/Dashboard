64d1c582ee99a371509dd9e54c02f0669f404fc7
Emilyempa <emilypettersson@hotmail.com>
2025-05-02 16:03:48 +0200

    Enhance dashboard page with revenue and invoice data visualization components

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -51,10 +51,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
+  try {      
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
